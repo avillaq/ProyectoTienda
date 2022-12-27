28d725be34ef7b1439b7463ca726dfdc8761db2f
avillaq <villafuertequispealex@gmail.com>
2022-12-27 18:15:50 -0500

    Sistema login mejorado

---
+++ cgi-bin/login.pl
@@ -0,0 +1,30 @@
+#!C:/xampp/perl/bin/perl.exe -w
+use strict;
+use CGI;
+use DBI;
+
+my $q = CGI->new;
+my $name = $q->param('name');
+my $password = $q->param('password');
+
+if($name eq "" || $password eq ""){
+    print "Location: ../login2.html \n\n";
+}
+else{
+    my $dsn = "DBI:mysql:database=datostienda;host=127.0.0.1";
+    my $dbh = DBI->connect($dsn, "Alex", "") or die "No se pudo conectar";
+    
+    my $sth = $dbh->prepare("SELECT * FROM usuario WHERE name=? AND password=?");
+    $sth->execute($name, $password);
+    
+    my @array = $sth->fetchrow_array;
+
+    $dbh->disconnect;
+
+    foreach my $row (@array) {
+        print "Name: $row";
+        print "Location: ./inicio.html \n\n";
+    }
+    print "Location: ../login2.html \n\n";
+
+}
